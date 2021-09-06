2f4efac473ade499016e14af8c45a5b7067e5188
JAAR91 <jaarriagaramos@gmail.com>
2021-09-06 17:01:28 -0600

    add linters and devserver to the project

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,24 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',z
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
 };
