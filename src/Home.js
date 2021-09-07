5c75de608b5d9072c5edf8200068a4a0f1e606bc
JAAR91 <jaarriagaramos@gmail.com>
2021-09-07 15:40:18 -0600

    add render home page and pagination to all the movies

--- src/Home.js
+++ src/Home.js
@@ -2,23 +2,115 @@ const mainContainer = document.getElementById('main');
 
 const renderHome = (movies) => {
     mainContainer.innerHTML = ``;
+
+    const homeContainer =  document.createElement('div');
+    homeContainer.classList.add('w-100', 'm-0', 
+    'p-0', 'bg-mywhite', 'p-3');
+    mainContainer.appendChild(homeContainer)
+
     const allMoviesContainer = document.createElement('div');
-    allMoviesContainer.classList.add('w-100', 'row', 'm-0', 'p-0');
-    mainContainer.appendChild(allMoviesContainer);
+    allMoviesContainer.classList.add('w-100', 'row', 'm-0', 
+    'p-0');
+    homeContainer.appendChild(allMoviesContainer);
+
+    let page = 1;
+    const renderMoviePage = (page) => {
+        allMoviesContainer.innerHTML = ``;
+
+        let end = page * 12;
+        let start = end - 13; 
+        movies.filter((item, index) => {
+            if(index > start && index < end){
+                return item;
+            }
+        }).forEach(movie => {
+            const movieSeparator = document.createElement('div');
+            movieSeparator.classList.add('col-3', 'm-0', 'p-2');
+            allMoviesContainer.appendChild(movieSeparator);
     
-    movies.forEach(movie => {
-        const movieSeparator = document.createElement('div');
-        movieSeparator.classList.add('col-3', 'm-0', 'p-2');
-        allMoviesContainer.appendChild(movieSeparator);
+            const movieContainer = document.createElement('div');
+            movieContainer.classList.add('w-100', 'border', 
+            'rounded', 'shadow', 'movieContainer');
+            movieSeparator.appendChild(movieContainer);
+    
+            const movieProfile = document.createElement('a');
+            movieProfile.href = '#';
+            movieProfile.innerHTML = `<a><img class="movie-image" src="${movie.image.medium}"></a>`;
+            movieContainer.appendChild(movieProfile);
+        });
+    };
 
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('w-100', 'border', 
-        'rounded', 'shadow', 'movieContainer');
-        movieSeparator.appendChild(movieContainer);
+    renderMoviePage(page);
 
-        movieContainer.innerHTML = `<p>${movie.name}</p>`;
+    const navigationContainer = document.createElement('nav');
+    navigationContainer.classList.add('Page', 'navigation');
+    homeContainer.appendChild(navigationContainer);
+
+    const pagination = document.createElement('ul');
+    pagination.classList.add('pagination', 'justify-content-center');
+    navigationContainer.appendChild(pagination);
+
+    const previousPage = document.createElement('li');
+    previousPage.classList.add('page-item', 'disabled');
+    previousPage.innerHTML = `<a href="#" class="page-link">Previous</a>`;
+    pagination.appendChild(previousPage);
+
+    previousPage.addEventListener('click', () => {
+        if(page > 1) {
+            page -= 1;
+            renderMoviePage(page);
+        }
     });
-   
+
+    for (let i = 1; i <  (movies.length/12); i++){
+        const pageNum = document.createElement('li');
+        if (page === i){
+            pageNum.classList.add('active');
+        }
+        pageNum.classList.add('page-item');
+        pageNum.innerHTML = `<a href="#" class="page-link">${i}</a>`;
+        pagination.appendChild(pageNum);
+
+        pageNum.addEventListener('click', () => {
+            renderMoviePage(i);
+            page = i;
+        });
+    }
+
+    const nextPage = document.createElement('li');
+    nextPage.classList.add('page-item');
+    nextPage.innerHTML = `<a href="#" class="page-link">Next</a>`;
+    pagination.appendChild(nextPage);
+
+    nextPage.addEventListener('click', () => {
+        if(page < (movies.length/12)) {
+            page += 1;
+            renderMoviePage(page);
+        }
+    });
+
+    pagination.addEventListener('click', () => {
+        pagination.querySelectorAll('a').forEach((item) => {
+            if(item.textContent == page) {
+                item.parentElement.classList.add('active');
+                console.log(item.textContent);
+            }else {
+                item.parentElement.classList.remove('active');
+            }
+
+            if (item.textContent == 'Previous' && page > 1){
+                item.parentElement.classList.remove('disabled');
+            } else if(item.textContent == 'Previous' && page < 2) {
+                item.parentElement.classList.add('disabled');
+            }
+            
+            if (item.textContent == 'Next' && page > (movies.length / 12 - 2)){
+                item.parentElement.classList.add('disabled');
+            } else if (item.textContent == 'Next' && page < (movies.length / 12)){
+                item.parentElement.classList.remove('disabled');
+            } 
+        });
+    });
 };
 
 export default renderHome;
