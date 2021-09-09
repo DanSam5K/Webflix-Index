fb8c9cc7ced87ce820b60426d000559de813812c
Daniel Samuel <dansam5000@gmail.com>
2021-09-09 17:36:17 +0100

    Add and display comment counter

--- src/displayComments.js
+++ src/displayComments.js
@@ -4,18 +4,20 @@ const displayComments = (comments) => {
   const loadedComment = document.createElement('ul');
   if (Array.isArray(comments)) {
     
-    loadedComment.classList.add('list-group', 'table-dark');
-    
+    loadedComment.classList.add('list-group','list-group-comments');
+    loadedComment.innerHTML += `
+    <li class="list-group-item"><p class="text-white text-center w-100 m-0">${comments.length} : Comments </p></li>
+    `
     comments.forEach((comment) => {
         loadedComment.innerHTML += `
-          <li class="list-group-item bg-dark">
+            <li class="list-group-item">
             <p class="text-white">${comment.username} (${comment.creation_date})</p>
             <span class="text-white">${comment.comment}</span>
           </li>
         `;
     })
   } else {
-    loadedComment.innerHTML += `<p class="text-white"> No comments </p>`;
+    loadedComment.innerHTML += `<li class="list-group-item bg-dark m-0"><p class="text-white text-center w-100 m-0"> 0 : Comments </p></li>`;
   }
 
   loadCommentsContainer.appendChild(loadedComment);
